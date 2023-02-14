14ba38f8178c0d005f82904b56cb093b660fc79c
Mehedi Ali <mehedi1234@hotmail.co.uk>
2023-02-14 16:37:15 +0000

    Add changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import "./Navbar.scss";
+import { FaAngellist } from "react-icons/fa";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      {" "}
+      <FaAngellist size={40} color='red' />
+      Navbar
+    </div>
+  );
 };
 
 export default Navbar;
