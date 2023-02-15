6c4b6cdab6a1d5b72ff68c9356ab2cfb60054115
Mehedi Ali <mehedi1234@hotmail.co.uk>
2023-02-15 19:05:24 +0000

    footer and project section complete

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,18 +6,21 @@ import shape from "../../assets/images/shape4.png";
 const Footer = () => {
   return (
     <footer className='footer'>
-      <h1 className='footer__title'>mehediali</h1>
-      <div className='footer__socials'>
-        <a href='https://github.com/mehediali12' target={"_blank"}>
-          <FaGithub className='footer__icon' size={22} />
-        </a>
-        <a
-          href='https://www.linkedin.com/in/mehedi-ali-profile/'
-          target={"_blank"}
-        >
-          <FaLinkedin className='footer__icon' size={22} />
-        </a>
+      <div className='footer__container'>
+        <h1 className='footer__title'>mehediali</h1>
+        <div className='footer__socials'>
+          <a href='https://github.com/mehediali12' target={"_blank"}>
+            <FaGithub className='footer__icon' size={22} />
+          </a>
+          <a
+            href='https://www.linkedin.com/in/mehedi-ali-profile/'
+            target={"_blank"}
+          >
+            <FaLinkedin className='footer__icon' size={22} />
+          </a>
+        </div>
       </div>
+
       <img className='footer__shape' src={shape} alt='' />
     </footer>
   );
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -5,79 +5,82 @@ const Projects = () => {
   return (
     <section className='projects'>
       <h1 className='projects__heading'>Projects</h1>
-      <div className='projects__project'>
-        <img className='projects__img' src={project} alt='' />
-        <div className='projects__details'>
-          <h1 className='projects__title'>STREAMING WEBSITE</h1>
-          <div className='projects__skills'>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JAVASCRIPT</p>
-          </div>
-          <div className='projects__links'>
-            <a className='projects__link' href='#'>
-              VIEW PROJECT
-            </a>
-            <a className='projects__link' href='#'>
-              VIEW CODE
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className='projects__project'>
-        <img className='projects__img' src={project} alt='' />
-        <div className='projects__details'>
-          <h1 className='projects__title'>ANIME DB WEBSITE</h1>
-          <div className='projects__skills'>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JAVASCRIPT</p>
-          </div>
-          <div className='projects__links'>
-            <a className='projects__link' href='#'>
-              VIEW PROJECT
-            </a>
-            <a className='projects__link' href='#'>
-              VIEW CODE
-            </a>
+
+      <div className='projects__container'>
+        <div className='projects__project'>
+          <img className='projects__img' src={project} alt='' />
+          <div className='projects__details'>
+            <h1 className='projects__title'>STREAMING WEBSITE</h1>
+            <div className='projects__skills'>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JAVASCRIPT</p>
+            </div>
+            <div className='projects__links'>
+              <a className='projects__link' href='#'>
+                VIEW PROJECT
+              </a>
+              <a className='projects__link' href='#'>
+                VIEW CODE
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='projects__project'>
-        <img className='projects__img' src={project} alt='' />
-        <div className='projects__details'>
-          <h1 className='projects__title'>REAL ESTATE WEBSITE</h1>
-          <div className='projects__skills'>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JAVASCRIPT</p>
-          </div>
-          <div className='projects__links'>
-            <a className='projects__link' href='#'>
-              VIEW PROJECT
-            </a>
-            <a className='projects__link' href='#'>
-              VIEW CODE
-            </a>
+        <div className='projects__project'>
+          <img className='projects__img' src={project} alt='' />
+          <div className='projects__details'>
+            <h1 className='projects__title'>ANIME DB WEBSITE</h1>
+            <div className='projects__skills'>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JAVASCRIPT</p>
+            </div>
+            <div className='projects__links'>
+              <a className='projects__link' href='#'>
+                VIEW PROJECT
+              </a>
+              <a className='projects__link' href='#'>
+                VIEW CODE
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='projects__project'>
-        <img className='projects__img' src={project} alt='' />
-        <div className='projects__details'>
-          <h1 className='projects__title'>QUIZ APP</h1>
-          <div className='projects__skills'>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>JAVASCRIPT</p>
+        <div className='projects__project'>
+          <img className='projects__img' src={project} alt='' />
+          <div className='projects__details'>
+            <h1 className='projects__title'>REAL ESTATE WEBSITE</h1>
+            <div className='projects__skills'>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JAVASCRIPT</p>
+            </div>
+            <div className='projects__links'>
+              <a className='projects__link' href='#'>
+                VIEW PROJECT
+              </a>
+              <a className='projects__link' href='#'>
+                VIEW CODE
+              </a>
+            </div>
           </div>
-          <div className='projects__links'>
-            <a className='projects__link' href='#'>
-              VIEW PROJECT
-            </a>
-            <a className='projects__link' href='#'>
-              VIEW CODE
-            </a>
+        </div>
+        <div className='projects__project'>
+          <img className='projects__img' src={project} alt='' />
+          <div className='projects__details'>
+            <h1 className='projects__title'>QUIZ APP</h1>
+            <div className='projects__skills'>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>JAVASCRIPT</p>
+            </div>
+            <div className='projects__links'>
+              <a className='projects__link' href='#'>
+                VIEW PROJECT
+              </a>
+              <a className='projects__link' href='#'>
+                VIEW CODE
+              </a>
+            </div>
           </div>
         </div>
       </div>
